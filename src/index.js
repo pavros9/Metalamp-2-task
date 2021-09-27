1791edcb1c17ee314de91e960477021d9acdc5a7
pavros9 <roman.pavlov.1996@bk.ru>
2021-09-27 12:45:04 +0300

    Working with JQuery

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import * as $ from "jquery";
 import Post from "./Post";
 import json from "./assets/json";
 import xml from "./assets/data.xml";
@@ -6,6 +7,8 @@ import Logo from "./assets/logo.png";
 import "./styles/style.css";
 
 const post = new Post("Webpack Post Title", Logo);
+
+$("pre").html(post.toString());
 
 console.log("Post to String", post.toString());
 
