3640da3e276a4a76d5a9c14eb7715ef679ca83f5
pavros9 <roman.pavlov.1996@bk.ru>
2021-09-27 22:21:06 +0300

    Added Css optimization

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,27 @@ const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all",
+    },
+    runtimeChunk: "single",
+  };
+
+  if (isProd) {
+    config.minimizer = [new CssMinimizerPlugin(), new TerserWebpackPlugin()];
+  }
+
+  return config;
+};
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -18,12 +38,7 @@ module.exports = {
   resolve: {
     extensions: [".js", ".json"],
   },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
-    runtimeChunk: "single",
-  },
+  optimization: optimization(),
   devServer: {
     open: true,
     port: 4200,
@@ -31,6 +46,9 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({
       template: "./index.html",
+      // minify: {
+      //   collapseWhitespace: isProd,
+      // },
     }),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
@@ -41,12 +59,21 @@ module.exports = {
         },
       ],
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          "css-loader",
+        ],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
