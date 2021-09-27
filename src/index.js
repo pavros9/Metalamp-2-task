e2556f23087875b7a6ac45c46559a8b55a6dc354
pavros9 <roman.pavlov.1996@bk.ru>
2021-09-27 12:21:39 +0300

    Working with CSV

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Post from "./Post";
 import json from "./assets/json";
 import xml from "./assets/data.xml";
+import csv from "./assets/data.csv";
 import Logo from "./assets/logo.png";
 import "./styles/style.css";
 
@@ -10,3 +11,4 @@ console.log("Post to String", post.toString());
 
 console.log("JSON:", json);
 console.log("XML:", xml);
+console.log("CSV:", csv);
