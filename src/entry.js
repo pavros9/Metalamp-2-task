63ac9260c85b282fce655c7bc989e2d4f288caeb
pavros9 <roman.pavlov.1996@bk.ru>
2021-10-21 13:07:56 +0300

    Fixed entry file( added require context with js files in common-blocks and pages) and fixed "Color" block

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,8 @@
 import "./style/global.scss";
+
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+
+requireAll(require.context("./common-blocks", true, /\.js$/));
+requireAll(require.context("./pages", true, /\.js$/));
