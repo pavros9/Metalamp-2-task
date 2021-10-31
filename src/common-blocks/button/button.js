7a1d0e75f9a505a279c641c957715e4a0bcb2a29
pavros9 <roman.pavlov.1996@bk.ru>
2021-10-31 15:25:09 +0300

    Button-like was added

--- src/common-blocks/button/button.js
+++ src/common-blocks/button/button.js
@@ -1,1 +1,16 @@
 import "./button.scss";
+
+window.addEventListener("click", function (event) {
+  let target = event.target;
+
+  if (target.matches(".button__like")) {
+    target.classList.toggle("button__like_active");
+    if (target.matches(".button__like_active")) {
+      let oldValue = target.innerHTML;
+      target.innerHTML = Number(oldValue) + 1;
+    } else {
+      let oldValue = target.innerHTML;
+      target.innerHTML = Number(oldValue) - 1;
+    }
+  }
+});
