54bbef78bae25f491dbeaa46020019c2002aaf8f
pavros9 <roman.pavlov.1996@bk.ru>
2021-11-13 14:10:19 +0300

    Fixed "button"

--- src/common-blocks/button/button.js
+++ src/common-blocks/button/button.js
@@ -4,14 +4,28 @@ import "./button.scss";
 window.addEventListener("click", function (event) {
   let target = event.target;
 
-  if (target.matches(".button__like")) {
-    target.classList.toggle("button__like_active");
-    if (target.matches(".button__like_active")) {
-      let oldValue = target.innerHTML;
-      target.innerHTML = Number(oldValue) + 1;
+  if (target.matches(".button_with-like")) {
+    target.classList.toggle("button_with-like_active");
+    let buttonText = target.querySelector(".button__text");
+    let oldValue = buttonText.innerHTML;
+    if (target.matches(".button_with-like_active")) {
+      buttonText.innerHTML = Number(oldValue) + 1;
     } else {
-      let oldValue = target.innerHTML;
-      target.innerHTML = Number(oldValue) - 1;
+      buttonText.innerHTML = Number(oldValue) - 1;
+    }
+  }
+  if (target.parentNode.matches(".button_with-like")) {
+    target.parentNode.classList.toggle("button_with-like_active");
+
+    let buttonText = target.parentNode.querySelector(".button__text");
+    let oldValue = buttonText.innerHTML;
+
+    if (target.parentNode.matches(".button_with-like_active")) {
+      buttonText.innerHTML = Number(oldValue) + 1;
+    } else {
+      buttonText.innerHTML = Number(oldValue) - 1;
     }
   }
 });
+
+// надо потом пофиксить
