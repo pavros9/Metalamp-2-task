9a87d6f2fa5f0b409e76f4acb2bae1d462a7901b
pavros9 <roman.pavlov.1996@bk.ru>
2021-11-28 12:45:24 +0300

    "dropdown", "form-item" were fixed

--- src/common-blocks/dropdown/dropdown.js
+++ src/common-blocks/dropdown/dropdown.js
@@ -2,9 +2,9 @@ import "./dropdown.scss";
 document.addEventListener("DOMContentLoaded", () =>
   window.addEventListener("click", function (event) {
     let target = event.target;
-    console.log(target);
+    // console.log(target);
     if (target.matches(".dropdown__main")) {
-      target.closest(".dropdown_").classList.toggle("show");
+      target.closest(".dropdown").classList.toggle("show");
     }
   })
 );
